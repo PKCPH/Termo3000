bea764b95bb1f97ad588762faa9bc21557cc04c1
PKCPH <patrick@klingaa.dk>
2023-09-27 12:44:37 +0200

    added workspace and enabled deep sleep in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 /*********
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com  
+  Complete project details at https://RandomNerdTutorials.com
 *********/
 
 #include <Arduino.h>
-//DS18B20 libraries
+// DS18B20 libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -21,19 +21,20 @@
 #include <SD.h>
 #include <SPI.h>
 
-//function declared
+// function declared
 void getReadings();
 void getTimeStamp();
 void logSDCard();
 void writeFile(fs::FS &fs, const char *path, const char *message);
-void appendFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char *path, const char *message);
+// void send();
 
 // GPIO where the DS18B20 is connected to
-const int oneWireBus = 4;     
+const int oneWireBus = 4;
 
 //////////SDCard
 // Define deep sleep options
-uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+uint64_t uS_TO_S_FACTOR = 1000000; // Conversion factor for micro seconds to seconds
 // Sleep for 10 minutes = 600 seconds
 uint64_t TIME_TO_SLEEP = 6;
 // Define CS pin for the SD card module
@@ -43,16 +44,18 @@ RTC_DATA_ATTR int readingID = 0;
 
 String dataMessage;
 
+unsigned long startMillis;
+const unsigned long sleepDuration = 300000; // 5 minutes in milliseconds
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(oneWireBus);
 
-// Pass our oneWire reference to Dallas Temperature sensor 
+// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 
 float currentTemperature = 0.0;
 
-//getting time
+// getting time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 // Variables to save date and time
@@ -63,32 +66,33 @@ String timeStamp;
 // Replace with your network credentials
 // const char* ssid = "E308";
 // const char* password = "98806829";
-const char* ssid = "Spiderman";
-const char* password = "@C4mpD3tS3jl3r!";
-
-
+const char *ssid = "Spiderman";
+const char *password = "@C4mpD3tS3jl3r!";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-String readBME280Temperature() {
+String readBME280Temperature()
+{
   // Read temperature as Celsius (the default)
   float t = sensors.getTempCByIndex(0);
-  if (isnan(t)) {    
+  if (isnan(t))
+  {
     Serial.println("Failed to read from DS18B20 sensor!");
   }
-  else {
+  else
+  {
     currentTemperature = t; // Update the current temperature
   }
   return String(currentTemperature);
 }
 
-void sendTemperatureToClients() {
+void sendTemperatureToClients()
+{
   String temperatureData = String(currentTemperature);
   ws.textAll(temperatureData);
 }
-
 
 // //besked fra client to server
 // void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
@@ -118,29 +122,31 @@ void sendTemperatureToClients() {
 //   }
 // }
 
-//starter websocket
-void initWebSocket() {
-  //ws.onEvent(onEvent);
+// starter websocket
+void initWebSocket()
+{
+  // ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
-
-void setup() {
+void setup()
+{
   // Start the Serial Monitor
   Serial.begin(115200);
 
- // Connect to Wi-Fi network with SSID and password
+  // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected.");
 
-    // Initialize a NTPClient to get time
+  // Initialize a NTPClient to get time
   timeClient.begin();
   // Set offset time in seconds to adjust for your timezone, for example:
   // GMT +1 = 3600
@@ -152,16 +158,17 @@ void setup() {
   // Start the DS18B20 sensor
   sensors.begin();
 
-
   // Initialize SPIFFS
-  if(!SPIFFS.begin()){
+  if (!SPIFFS.begin())
+  {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
 
-    // Connect to Wi-Fi
+  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
@@ -171,91 +178,111 @@ void setup() {
 
   initWebSocket();
 
-// Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html");
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readBME280Temperature().c_str());
-  });
+ 
 
-  
-  // Start server
-  server.begin();
-
-// Initialize SD card
-  SD.begin(SD_CS);  
-  if(!SD.begin(SD_CS)) {
+  // Initialize SD card
+  SD.begin(SD_CS);
+  if (!SD.begin(SD_CS))
+  {
     Serial.println("Card Mount Failed");
     return;
   }
   uint8_t cardType = SD.cardType();
-  if(cardType == CARD_NONE) {
+  if (cardType == CARD_NONE)
+  {
     Serial.println("No SD card attached");
     return;
   }
   Serial.println("Initializing SD card...");
-  if (!SD.begin(SD_CS)) {
+  if (!SD.begin(SD_CS))
+  {
     Serial.println("ERROR - SD card initialization failed!");
-    return;    // init failed
+    return; // init failed
   }
+
+
 
   // If the data.txt file doesn't exist
   // Create a file on the SD card and write the data labels
   File file = SD.open("/data.txt");
-  if(!file) {
+  if (!file)
+  {
     Serial.println("File doens't exist");
     Serial.println("Creating file...");
     writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
   }
-  else {
-    Serial.println("File already exists");  
+  else
+  {
+    Serial.println("File already exists");
   }
   file.close();
+
+   // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html"); });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/plain", readBME280Temperature().c_str()); });
+ server.on("/downloadCSV", HTTP_GET, [](AsyncWebServerRequest *request){
+  File file = SD.open("/data.txt");
+  if (!file) {
+     request->send(404, "text/plain", "CSV file not found");
+     } else {
+      request->send(SD, "/data.txt", "text/csv", false);
+      file.close();
+    }
+  });
+
+
+  // Start server
+  server.begin();
 
   // Enable Timer wake_up
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
   // Start the DallasTemperature library
-  sensors.begin(); 
+  sensors.begin();
 
   getReadings();
   getTimeStamp();
   logSDCard();
-  
+
   // Increment readingID on every new reading
   readingID++;
-  
-  // Start deep sleep
-  Serial.println("DONE! Going to sleep now.");
-  //esp_deep_sleep_start();
 
+  // Initialize your setup code here
+
+  // Record the start time
+  startMillis = millis();
 }
 
-void loop() {
-  // sensors.requestTemperatures(); 
-  // float temperatureC = sensors.getTempCByIndex(0);
-  // if (!isnan(temperatureC) && currentTemperature != temperatureC) {
-  //   currentTemperature = temperatureC;
-  // sendTemperatureToClients(); // Send temperature data to clients
-  // Serial.print(currentTemperature);
-  // Serial.println("ºC");
-  // }
-  // delay(1000);
+void loop()
+{
+  // Check if 5 minutes (300,000 milliseconds) have passed
+  if (millis() - startMillis >= sleepDuration)
+  {
+    // 5 minutes have passed, so initiate deep sleep
+    // Start deep sleep
+    Serial.println("DONE! Going to sleep now.");
+    esp_deep_sleep_start();
+  }
+  // Your other loop code (if any) can go here
 }
 
 // Function to get temperature
-void getReadings(){
-  sensors.requestTemperatures(); 
+void getReadings()
+{
+  sensors.requestTemperatures();
   currentTemperature = sensors.getTempCByIndex(0); // Temperature in Celsius
-  //temperature = sensors.getTempFByIndex(0); // Temperature in Fahrenheit
+  // temperature = sensors.getTempFByIndex(0); // Temperature in Fahrenheit
   Serial.print("Temperature: ");
   Serial.println(currentTemperature);
 }
 
 // Function to get date and time from NTPClient
-void getTimeStamp() {
-  while(!timeClient.update()) {
+void getTimeStamp()
+{
+  while (!timeClient.update())
+  {
     timeClient.forceUpdate();
   }
   // The formattedDate comes with the following format:
@@ -269,49 +296,58 @@ void getTimeStamp() {
   dayStamp = formattedDate.substring(0, splitT);
   Serial.println(dayStamp);
   // Extract time
-  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
   Serial.println(timeStamp);
 }
 
-
-
 // Write to the SD card (DON'T MODIFY THIS FUNCTION)
-void writeFile(fs::FS &fs, const char * path, const char * message) {
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
   Serial.printf("Writing file: %s\n", path);
 
   File file = fs.open(path, FILE_WRITE);
-  if(!file) {
+  if (!file)
+  {
     Serial.println("Failed to open file for writing");
     return;
   }
-  if(file.print(message)) {
+  if (file.print(message))
+  {
     Serial.println("File written");
-  } else {
+  }
+  else
+  {
     Serial.println("Write failed");
   }
   file.close();
 }
 
 // Append data to the SD card (DON'T MODIFY THIS FUNCTION)
-void appendFile(fs::FS &fs, const char * path, const char * message) {
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
   Serial.printf("Appending to file: %s\n", path);
 
   File file = fs.open(path, FILE_APPEND);
-  if(!file) {
+  if (!file)
+  {
     Serial.println("Failed to open file for appending");
     return;
   }
-  if(file.print(message)) {
+  if (file.print(message))
+  {
     Serial.println("Message appended");
-  } else {
+  }
+  else
+  {
     Serial.println("Append failed");
   }
   file.close();
 }
 
 // Write the sensor readings on the SD card
-void logSDCard() {
-  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + 
+void logSDCard()
+{
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," +
                 String(currentTemperature) + "\r\n";
   Serial.print("Save data: ");
   Serial.println(dataMessage);
